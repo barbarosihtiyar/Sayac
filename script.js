25851bb20ccbada96a540f1af1ae965d36590b00
Barbaros İhtiyar <104565169+barbarosihtiyar@users.noreply.github.com>
2022-07-06 21:03:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const sayacsaniye=document.getElementById("sayacsaniye");
 
 const baslat=document.getElementById("baslat");
 const sifirla=document.getElementById("sifirla");
+ 
+let dur=false; 
 
 sayacdakika.addEventListener("change", () => {
     if(sayacdakika.value < 10){
@@ -27,7 +29,13 @@ else{
 });
 
 baslat.addEventListener("click",sayacbasla);
-
+sifirla.addEventListener("click",() => {
+    dur = true;
+    dakika.textContent="00";
+    saniye.textContent="00";
+    sayacsaniye.value="00";
+    sayacdakika.value="00"; 
+})
     function sayacbasla(){
     let minute = dakika.textContent;
     let second = saniye.textContent;
@@ -38,6 +46,16 @@ baslat.addEventListener("click",sayacbasla);
         if(second == -1){
             minute--;
             second=59;
+        }
+        if((minute == 0 && second==0) || (minute==00 && second==0) ){
+            clearInterval(interval);
+            window.alert("Süreniz Doldu");
+            sayacdakika.value=="00";
+            sayacsaniye.value=="00";
+        }
+        if(dur){
+            clearInterval(interval);
+            return;
         }
        dakika.textContent = minute;
        saniye.textContent = second;
